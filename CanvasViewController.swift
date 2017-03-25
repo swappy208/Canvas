f01df60f1f38649e1dbdddaa20c09b153c83e5c0
swappy208@gmail.com <swappy208@gmail.com>
2017-03-25 01:36:15 -0400

    completed milestone 5

---
+++ CanvasViewController.swift
@@ -0,0 +1,94 @@
+//
+//  CanvasViewController.swift
+//  Canvas
+//
+//  Created by Swapnil Tamrakar on 3/24/17.
+//
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+
+    @IBOutlet weak var trayView: UIView!
+    var newlyCreatedFaceOriginalCenter: CGPoint!
+    var newlyCreatedFace: UIImageView!
+    var trayOriginalCenter: CGPoint!
+    var trayDownOffset: CGFloat!
+    var trayUp: CGPoint!
+    var trayDown: CGPoint!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newlyCreatedFace.isUserInteractionEnabled = true
+        trayDownOffset = 160
+        trayUp = trayView.center
+        trayDown = CGPoint(x: trayView.center.x ,y: trayView.center.y + trayDownOffset)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func didPanTray(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        if sender.state == .began {
+            print("Gesture began")
+            trayOriginalCenter = trayView.center
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+            trayView.center = CGPoint(x: trayOriginalCenter.x, y: trayOriginalCenter.y + translation.y)
+        } else if sender.state == .ended {
+            print("Gesture ended")
+            var velocity = sender.velocity(in: view)
+            if velocity.y > 0
+            {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayDown
+                }, completion: nil)
+            }
+            else
+            {
+                UIView.animate(withDuration:0.4, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options:[] ,
+                               animations: { () -> Void in
+                                self.trayView.center = self.trayUp
+                }, completion: nil)
+            }
+        }
+    }
+
+    @IBAction func didPanFace(_ sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        var imageView = sender.view as! UIImageView
+        if sender.state == .began {
+            print("Gesture began")
+            newlyCreatedFace = UIImageView(image: imageView.image)
+            view.addSubview(newlyCreatedFace)
+            newlyCreatedFace.center = imageView.center
+            newlyCreatedFace.center.y += trayView.frame.origin.y
+            newlyCreatedFaceOriginalCenter = newlyCreatedFace.center
+            let panGestureRecognizer = UIPanGestureRecognizer(target: self, action: #selector(didPanFace))
+            // Attach it to a view of your choice. If it's a UIImageView, remember to enable user interaction
+            imageView.isUserInteractionEnabled = true
+            imageView.addGestureRecognizer(panGestureRecognizer)
+        } else if sender.state == .changed {
+            print("Gesture is changing")
+            newlyCreatedFace.center = CGPoint(x: newlyCreatedFaceOriginalCenter.x + translation.x, y: newlyCreatedFaceOriginalCenter.y + translation.y)
+            // The didTap: method will be defined in Step 3 below.
+        } else if sender.state == .ended {
+            print("Gesture ended")
+        }
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
